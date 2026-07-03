9d9879bfa678a4356d49fc0f33d7371e92fae467
Surya Teja <suryateja@example.com>
2026-07-03 12:55:49 +0530

    Updated css

--- src/components/gallery/Photoslanding.js
+++ src/components/gallery/Photoslanding.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 import '../../styles/Photosection.css';
 import Photosection from "./Photosection";
 import Picsroutes from "./Picsroutes";
+
+
 const Photoslanding = () => {
+    useEffect(() => {
+  window.scrollTo(0, 0);
+  console.log("PhotosLanding mounted");
+}, []);
+ 
     return (
         <div style={{backgroundColor:"#FFF7D9"}}>
             <Photosection />
